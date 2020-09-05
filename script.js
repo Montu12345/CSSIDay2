f966b18eab39d204cb543ed863d1e7c5888477c1
Montu12345 <48189882+Montu12345@users.noreply.github.com>
2020-09-05 15:52:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ let dvdImage;
 
 function setup(){
   createCanvas(800, 600);
+  
   // We only want to load the logo once.
   dvdImage = loadImage("https://cdn.glitch.com/eaea72a4-ac6d-4777-b76e-f37d75959aa5%2Fdvd.jpeg?1515761833387");
-  masterVelocity = 3
+ 
+  
+  //setting the dimentions of the image
   imageWidth = 200
   imageHeight = 150
+  
+  //velocities of how fast the image moves around the screen
+  masterVelocity = 3
   xVelocity = masterVelocity;
   yVelocity = masterVelocity;
+  
   x = 50;
   y = 50;
   background(220);
@@ -16,38 +23,25 @@ function setup(){
 
 function draw(){
   
+  //if the x-position of the image is outside of the canvas dimentions, make the image bounce back by changing the velocity
   if (x > (width - imageWidth)){
     xVelocity = -masterVelocity;
   }
   else if (x < 0) {
     xVelocity = masterVelocity;
   }
+  x += xVelocity;
   
+  //if the y-position of the image is outside of the canvas dimentions, make the image bounce back by changing the velocity
   if (y > (height - imageHeight)){
     yVelocity = -masterVelocity;
   }
   else if (y < 0) {
     yVelocity = masterVelocity;
   }
-  
-  x += xVelocity;
   y += yVelocity;
-  
   
   // Draw the logo at the new position.
   image(dvdImage, x, y, imageWidth, imageHeight);
   image(cakeImage, 400, 400, imageWidth, imageHeight)
 }
-
-/*
-if (x>600 || y>(600-150)){
-    xVelocity = -3;
-    yVelocity = -3;
-  }
-  else if (x < 0 || y < 0) {
-    xVelocity = 3;
-    yVelocity = 3;
-  }
-  x += xVelocity;
-  y += yVelocity;
-*/
